1de3a3c9a1b656eafa1e4446e9ef57a0e20dad52
chris48s <c.mshaw@yahoo.co.uk>
2016-09-14 19:31:58 +0100

    Scrape stations from xml instead of json
    ..so that we get consistent hash values

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from dc_base_scrapers.ckan_scraper import CkanScraper
 from dc_base_scrapers.geojson_scraper import GeoJsonScraper
+from dc_base_scrapers.xml_scraper import Wfs2Scraper
 
 
 base_url = 'https://salforddataquay.uk/api/3/action/package_show?id='
@@ -25,7 +26,18 @@ stations_meta_scraper = CkanScraper(
     stations_info['return_format'],
     stations_info['extra_fields'],
     'utf-8')
-stations_url = stations_meta_scraper.scrape()
+stations_meta_scraper.scrape()
+stations_url = "http://map.salford.gov.uk/geoserver/OpenData/ows?service=wfs&version=2.0.0&request=GetFeature&typeNames=OpenData%3Av_polling_stations&srsName=EPSG%3A4326"
+stations_fields = {
+    '{http://www.salford.gov.uk/maps/OpenData}id': 'id',
+    '{http://www.salford.gov.uk/maps/OpenData}ward': 'ward',
+    '{http://www.salford.gov.uk/maps/OpenData}polling_district': 'polling_district',
+    '{http://www.salford.gov.uk/maps/OpenData}station_location': 'station_location',
+    '{http://www.salford.gov.uk/maps/OpenData}easting': 'easting',
+    '{http://www.salford.gov.uk/maps/OpenData}northing': 'northing',
+    '{http://www.salford.gov.uk/maps/OpenData}longitude_wgs_84': 'longitude_wgs_84',
+    '{http://www.salford.gov.uk/maps/OpenData}latitude_wgs_84': 'latitude_wgs_84',
+}
 
 districts_meta_scraper = CkanScraper(
     base_url,
@@ -37,8 +49,8 @@ districts_url = districts_meta_scraper.scrape()
 
 
 if stations_url:
-    stations_scraper = GeoJsonScraper(
-        stations_url, council_id, 'utf-8', 'stations')
+    stations_scraper = Wfs2Scraper(
+        stations_url, council_id, 'stations', stations_fields, 'id')
     stations_scraper.scrape()
 
 if districts_url:
